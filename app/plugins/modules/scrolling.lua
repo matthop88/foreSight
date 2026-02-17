73a140d27c0fae84ffacd17380b76861c5945d7d
Matthew Hopkins <matthop88@gmail.com>
2026-02-17 15:39:00 -0800

    Create scrolling.lua plugin module

---
+++ app/plugins/modules/scrolling.lua
@@ -0,0 +1,57 @@
+local SCROLL_SPEED = 400
+
+return {
+    xSpeed   = 0,
+    ySpeed   = 0,
+    leftKey  = "left",
+    rightKey = "right",
+    upKey    = "up",
+    downKey  = "down",
+
+    init = function(self, params)
+        self.imageViewer = params.imageViewer
+        self.leftKey     = params.leftKey     or self.leftKey
+        self.rightKey    = params.rightKey    or self.rightKey
+        self.upKey       = params.upKey       or self.upKey
+        self.downKey     = params.downKey     or self.downKey
+        self.scrollSpeed = params.scrollSpeed or SCROLL_SPEED
+        return self
+    end,
+
+    handleKeypressed = function(self, key)
+        if     key == self.leftKey  then self:startLeft()  return true
+        elseif key == self.rightKey then self:startRight() return true
+        elseif key == self.upKey    then self:startUp()    return true
+        elseif key == self.downKey  then self:startDown()  return true
+        end
+    end,
+
+    handleKeyreleased = function(self, key)
+        if     key == self.leftKey  then self:stopLeft()   return true
+        elseif key == self.rightKey then self:stopRight()  return true
+        elseif key == self.upKey    then self:stopUp()     return true
+        elseif key == self.downKey  then self:stopDown()   return true
+        end
+    end,
+
+    update = function(self, dt)
+        self.imageViewer:moveImage(self.xSpeed * dt, self.ySpeed * dt)
+        if self:isMotionless() and self.imageViewer.keepImageInBounds then
+            self.imageViewer:keepImageInBounds()
+        end
+    end,
+
+    startLeft  = function(self) self.xSpeed =  self.scrollSpeed end,
+    startRight = function(self) self.xSpeed = -self.scrollSpeed end,
+    startUp    = function(self) self.ySpeed =  self.scrollSpeed end,
+    startDown  = function(self) self.ySpeed = -self.scrollSpeed end,
+
+    stopLeft   = function(self) self.xSpeed = math.min(0, self.xSpeed) end,
+    stopRight  = function(self) self.xSpeed = math.max(0, self.xSpeed) end,
+    stopUp     = function(self) self.ySpeed = math.min(0, self.ySpeed) end,
+    stopDown   = function(self) self.ySpeed = math.max(0, self.ySpeed) end,
+
+    isMotionless = function(self)
+        return self.xSpeed == 0 and self.ySpeed == 0
+    end,
+}
