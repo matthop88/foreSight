571332df8e81625dc6e5aee33c2f9793a0764ef7
Matthew Hopkins <matthop88@gmail.com>
2026-02-18 12:10:42 -0800

    Added nodeSelector.lua to plugins

---
+++ app/plugins/modules/nodeSelector.lua
@@ -0,0 +1,24 @@
+return {
+    init = function(self, params)
+        self.gfx   = params.gfx
+        self.nodes = params.nodes
+        return self
+    end,
+
+    update = function(self, dt)
+        local wx, wy = self.gfx:screenToImageCoordinates(love.mouse.getPosition())
+        for _, node in ipairs(self.nodes) do
+            node.hovered = node:containsPoint(wx, wy)
+        end
+    end,
+
+    handleMousepressed = function(self, mx, my, params)
+        local wx, wy = self.gfx:screenToImageCoordinates(mx, my)
+        local hit = false
+        for _, node in ipairs(self.nodes) do
+            node.selected = node:containsPoint(wx, wy)
+            if node.selected then hit = true end
+        end
+        if hit then return true end
+    end,
+}
