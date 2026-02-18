c707bc755b458edf13be2066fa6da9e0dd77e063
Matthew Hopkins <matthop88@gmail.com>
2026-02-18 12:12:59 -0800

    Updated node.lua so it has a containsPoint() method

--- app/diagram/node.lua
+++ app/diagram/node.lua
@@ -11,17 +11,34 @@ function Node.new(params)
         color    = params.color    or { 0.3, 0.55, 1.0 },
         children = {},
         edges    = {},
+        hovered  = false,
+        selected = false,
+
+        containsPoint = function(self, wx, wy)
+            return wx >= self.x and wx < self.x + self.w
+               and wy >= self.y and wy < self.y + self.h
+        end,
 
         draw = function(self, gfx)
             local r, g, b = self.color[1], self.color[2], self.color[3]
 
-            -- Fill
-            gfx:setColor(r, g, b, 0.18)
+            -- Fill: more opaque when hovered or selected
+            local fillAlpha = 0.18
+            if     self.selected then fillAlpha = 0.45
+            elseif self.hovered  then fillAlpha = 0.30
+            end
+            gfx:setColor(r, g, b, fillAlpha)
             gfx:rectangle("fill", self.x, self.y, self.w, self.h)
 
-            -- Border
-            gfx:setColor(r, g, b, 1)
+            -- Border: white when selected, node color otherwise
+            if self.selected then
+                gfx:setColor(1, 1, 1, 1)
+            else
+                gfx:setColor(r, g, b, 1)
+            end
+            gfx:setLineWidth(self.selected and 2 or 1)
             gfx:rectangle("line", self.x, self.y, self.w, self.h)
+            gfx:setLineWidth(1)
 
             -- Label, vertically centered
             gfx:setFontSize(14)
