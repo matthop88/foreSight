a6d65edd37490f5247fdfddc1bd63e714ba57557
Matthew Hopkins <matthop88@gmail.com>
2026-02-18 12:32:09 -0800

    Updated main.lua to refer to nodes.lua data file

--- app/main.lua
+++ app/main.lua
@@ -13,13 +13,7 @@ require("app/plugins/engine")
     :add("zooming",     { imageViewer = gfx })
     :add("nodeManager", {
         gfx   = gfx,
-        nodes = {
-            { id = "power",   label = "Power System",   x =  60, y = 100, w = 200, h = 110, color = { 0.3,  0.55, 1.0  } },
-            { id = "control", label = "Control System", x = 340, y = 100, w = 200, h = 110, color = { 0.2,  0.75, 0.5  } },
-            { id = "sensors", label = "Sensors",        x =  60, y = 290, w = 200, h = 110, color = { 0.9,  0.55, 0.2  } },
-            { id = "actuate", label = "Actuators",      x = 340, y = 290, w = 200, h = 110, color = { 0.85, 0.3,  0.3  } },
-            { id = "ui",      label = "User Interface", x = 620, y = 195, w = 200, h = 110, color = { 0.7,  0.4,  0.9  } },
-        },
+        nodes = require("app/diagram/nodes"),
     })
     :add("questionBox",
         {   x                = 1150,
